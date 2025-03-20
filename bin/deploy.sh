ed2bcc8f515268245b5dbf11a1825f3ebdf5e02d
shadowbrush <github@shadowbrush.com>
2025-03-19 18:40:45 -0700

    Docker config

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,14 +6,19 @@ REMOTE_HOST=mmhealth
 rsync -avPe ssh --delete \
 --exclude "*.md" \
 --exclude "test/*" \
---exclude "*/docs/*" \
 src \
 package.json \
 tsconfig.json \
+.dockerignore \
+Dockerfile \
+docker-compose.yaml \
 ${REMOTE_HOST}:apps/servicepulse/
 
 # To copy over the .env file (if needed):
 # scp env/.env.production ${REMOTE_HOST}:apps/servicepulse/.env
+
+# To copy over the config file (if needed):
+# scp config/*.json ${REMOTE_HOST}:apps/servicepulse/config/
 
 # Building the app:
 ssh ${REMOTE_HOST} "cd apps/servicepulse && npm install && npm run build"
---
+++ bin/reboot-healthmonitor.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+aws ec2 reboot-instances --instance-ids i-0bfb64d728b7d57f5
